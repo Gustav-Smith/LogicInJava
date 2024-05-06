d7ee46a35a79237fe44dd7e886b4ea48705c03d2
Gustav-Smith <gustavojeanpt@gmail.com>
2024-05-06 15:33:55 -0300

    AgeDifferenceCalculator

---
+++ src/AgeDifferenceCalculator.java
@@ -0,0 +1,8 @@
+public class AgeDifferenceCalculator {
+    public static void main(String[] args) {
+        int firstAge = 30;
+        int secondAge = 30;
+        int ageDifference = firstAge - secondAge;
+        System.out.println("The age difference is " + ageDifference);
+    }
+}
