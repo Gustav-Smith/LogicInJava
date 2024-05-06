45909d411b3580452348f0c04bd10353bb7afd6a
Gustav-Smith <gustavojeanpt@gmail.com>
2024-05-06 17:38:49 -0300

    CheckAge

---
+++ src/CheckAge.java
@@ -0,0 +1,10 @@
+public class CheckAge {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("adult");
+        } else {
+            System.out.println("under age");
+        }
+    }
+}
